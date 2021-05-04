c262bd936a3500f494ad8d5b4bfe5d5f0043d932
adbonino <abonino88@gmail.com>
2021-05-04 13:30:23 -0300

    agregue un while para ller comandoa [iss53]

--- pruebas/leer_datos.sh
+++ pruebas/leer_datos.sh
@@ -6,4 +6,18 @@ linea=`grep "APELLIDO" $ARCH`
 APELLIDO=`echo $linea | cut -d "=" -f 1`
 
 echo "apellido: $APELLIDO"
+echo
+
+echo "listado de parametros y valores"
+while read LINEA
+    do  
+	if [[ -n $LINEA ]]
+	    then
+            PARAMETRO=`echo $LINEA | cut -d "=" -f 1`
+            VALOR=`echo $LINEA | cut -d "=" -f 2`
+
+            echo "Nombre del parametro: $PARAMETRO, valor: $VALOR"
+        fi	    
+    done < $ARCH
+
 

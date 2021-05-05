2d6d84006cd82a0d4c8b5eeb54f3b8b9811023d8
Adbonino <abonino88@gmail.com>
2021-05-04 22:37:26 -0300

    Create convertir_tmp.sh

---
+++ entregas/convertir_tmp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ******************************************************************
+# Universidad de Buenos Aires
+# Facultad de Ingenieria
+#
+# 75.08 Sistemas Operativos
+# Catedra Ing. Osvaldo Clua
+#
+#
+# Ejemplo de Comportamiento de un ciclo while para lectura de un achivo
+# Autor: Lic. Adrian Muccio
+#
+# ******************************************************************
